2e2949c0dbea88678fb34e1399bdeec21f80e390
smcnab1 <sammcnab.email@gmail.com>
2022-09-08 22:10:12 +0100

    Config Spring Cleaning

--- entities/gitpush/ha_gitpush.sh
+++ entities/gitpush/ha_gitpush.sh
@@ -1,5 +1,12 @@
-# Go to /config folder or 
-# Change this to your Home Assistant config folder if it is different
+#       _ _                  _                 _      _   
+#  __ _(_) |_   _ __ _  _ __| |_    ___ __ _ _(_)_ __| |_ 
+# / _` | |  _| | '_ \ || (_-< ' \  (_-</ _| '_| | '_ \  _|
+# \__, |_|\__| | .__/\_,_/__/_||_| /__/\__|_| |_| .__/\__|
+# |___/        |_|                              |_|       
+#--FROM https://github.com/smcnab1/op-question-mark
+#---Script to push regular commit to github repo to keep it up to date.
+
+# Go to /config folder
 cd /config
 
 # Add all files to the repository with respect to .gitignore rules
